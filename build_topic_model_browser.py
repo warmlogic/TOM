ee382a8398ea4781e97ad9d6f1fa34cf586625e9
Matt Mollison <matt.mollison@gmail.com>
2019-06-02 18:09:26 +0300

    turn off debugger

--- build_topic_model_browser.py
+++ build_topic_model_browser.py
@@ -448,5 +448,5 @@ def hashed_static_file(endpoint, values):
 if __name__ == '__main__':
     # Access the browser at http://localhost:2016/
     # app.run(debug=True, host='localhost', port=2016)
-    app.run(debug=True, host='0.0.0.0', port=5000)
-    # app.run(debug=False, host='0.0.0.0', port=5000)
+    # app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=5000)
