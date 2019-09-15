68f0c5d0ee482e4e8e966782ac23d15e75b47dbb
Matt Mollison <matt.mollison@gmail.com>
2019-09-15 15:20:47 -0700

    chunks function

--- tom_lib/utils.py
+++ tom_lib/utils.py
@@ -105,3 +105,9 @@ def delete_folder(pth):
         else:
             sub.unlink()
     pth.rmdir()
+
+
+def chunks(l, n):
+    """Yield successive n-sized chunks from l."""
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
